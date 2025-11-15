bb52f4594c09b521d313619942a0968ea12482b8
Dasha2002 <kosolapova.dasha2018@yandex.ru>
2025-11-15 16:58:43 +0500

    Добавила новую функциональность X

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
                 <li class="nav-li"><a class="link" href="#">Популярные вопросы</a></li>
             </ul>
         </nav>
-        
+
     </header>
 
     <section class="banner-debt">
@@ -73,22 +73,153 @@
 
     <section class="services">
         <div class="block">
+
             <h1>Наши услуги</h1>
-            <div class="block-services">
-                <div class="block-1-first-row"></div>
-                <div class="block-2-first-row"></div>
 
-                <div class="block-3-second-row"></div>
-                <div class="block-4-second-row"></div>
-                <div class="block-5-second-row"></div>
+            <div class="bloc-one">
 
-                <div class="block-6-third-row"></div>
-                <div class="block-7-third-row"></div>
-                <div class="block-8-third-row"></div>
+                <div class="block-1-first-row">
+                    <div class="overlay-img">
+                        <img src="/assets/img/Rectangle 18.svg" alt="Передний план">
+                    </div>
 
-                <div class="block-9-fourth-row"></div>
-                <div class="block-10-fourth-row"></div>
-                <div class="block-11-fourth-row"></div>
+                    <div class="text-overlay">
+                        <h2>Банкротство физических лиц</h2>
+                        <p>Мы поможем избавиться от кредитов и<br> микрозаймов на основе закона №127-<br> ФЗ</p>
+                    </div>
+
+                    <div class="btn-overlay">
+                        <button class="btn-application">Оставить заявку</button>
+                        <button class="btn-detail">Подробнее</button>
+                    </div>
+
+                </div>
+
+                <div class="block-2-first-row">
+                    <a hreef="#">
+                        <div class="overlay-img">
+                            <img src="/assets/img/main-services-one.svg">
+                        </div>
+                        <div class="block">
+                            <img src="/assets/img/detail-blue-one.svg" class="img-blue-detail">
+                        <h2>Банкротство<br> индивидуальных<br> предпринимателей</h2>
+                        </div>
+                        
+                    </a>
+                </div>
+            </div>
+
+
+
+            <div class="block-two">
+                <div class="block-3-second-row">
+                    <a href="#">
+                        <div>
+                            <img src="/assets/img/detail-icon.svg">
+                            <h2>Банкротство с гарантией</h2>
+                        </div>
+                    </a>
+                </div>
+                <div class="block-4-second-row">
+                    <a href="#">
+                        <div>
+                            <img src="/assets/img/detail-icon.svg">
+                            <h2>Банкротство в рассрочку</h2>
+                        </div>
+                    </a>
+                </div>
+                <div class="block-5-second-row">
+                    <a href="#">
+                        <div>
+                            <img src="/assets/img/detail-icon.svg">
+                            <h2>Помощь должникам</h2>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="block-6-third-row">
+                    <a href="#">
+                        <div>
+                            <img src="/assets/img/detail-icon.svg">
+                            <h2>Сопровождение банкротства</h2>
+                        </div>
+                    </a>
+                </div>
+                <div class="block-7-third-row">
+                    <a href="#">
+                        <div>
+                            <img src="/assets/img/detail-icon.svg">
+                            <h2>Дистанционное банкротства</h2>
+                        </div>
+                    </a>
+                </div>
+                <div class="block-8-third-row">
+                    <a href="#">
+                        <div>
+                            <img src="/assets/img/detail-icon.svg">
+                            <h2>Банкротство с ипотекой</h2>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="block-9-fourth-row">
+                    <a href="#">
+                        <div>
+                            <img src="/assets/img/detail-icon.svg">
+                            <h2>Банкротство по налогам</h2>
+                        </div>
+                    </a>
+                </div>
+                <div class="block-10-fourth-row">
+                    <a href="#">
+                        <div>
+                            <img src="/assets/img/detail-icon.svg">
+                            <h2>Банкротство по штрафам</h2>
+                        </div>
+                    </a>
+                </div>
+                <div class="block-11-fourth-row">
+                    <a href="#">
+                        <div>
+                            <img src="/assets/img/detail-icon.svg">
+                            <h2>Банкротство пенсионеров</h2>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="csd">
+        <div class="block">
+            <img src="/assets/img/img-csd.svg">
+            <h1>ЦСД - ваш проводник в мир без долгов</h1>
+            <div class="block-csd">
+                <div class="block-one">
+                    <h1>Гарантия</h1>
+                    <p>Мы беремся только за дело,<br> результаты которого можем<br> гарантировать</p>
+                </div>
+                <div class="block-one">
+                    <h1>Подготовка</h1>
+                    <p>Перед началом работы, наши<br> юристы тщательно изучат ваше<br> дело и подготовят план работы</p>
+                </div>
+                <div class="block-one">
+                    <h1>Время</h1>
+                    <p>Работая с нами, вам не придется<br> посещать суды - мы сделаем это<br> за вас</p>
+                </div>
+                <div class="block-one">
+                    <h1>Сопровождение</h1>
+                    <p>За вами будет закреплен специалист,<br> который будет сопровождать<br> процедуру до списания всех долгов</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="write-downs">
+        <div class="block">
+            <div class="img">
+                <img src="">
             </div>
         </div>
     </section>
